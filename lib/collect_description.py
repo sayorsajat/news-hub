da43115079d81b507f9dc9838843442b3c653168
sayorsajat <selion.forc@gmail.com>
2023-09-05 07:04:44 +0200

    new technology try

--- lib/collect_description.py
+++ lib/collect_description.py
@@ -1,20 +1,22 @@
 from data_util.writeNews import writeDescription
-from newspaper import Article, Config
+import requests
+from bs4 import BeautifulSoup
+import gensim
+from gensim.summarization import summarize
 
 def sanitize_text(text):
     # Remove null characters because they are crashing writing to database
     return ''.join(c for c in text if c.isprintable() or c.isspace())
 
 def collect_description_dynamic(session, NewsTable, URL, ID):
-    USER_AGENT = "Mozilla/5.0 (X11; Linux i686; rv:109.0) Gecko/20100101 Firefox/115.0"
-    config = Config()
-    config.browser_user_agent = USER_AGENT
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:109.0) Gecko/20100101 Firefox/115.0"}
+
+    page = requests.get(URL, headers=headers)
+    
+    soup = BeautifulSoup(page.content, 'html.parser')
 
     try:
-        article = Article(URL, config=config)
-
-        article.download()
-        article.parse()
+        article_text = soup.get_text()
     except:
         print("no access to url")
         writeDescription("error while collecting", ID, session, NewsTable)
@@ -22,8 +24,8 @@ def collect_description_dynamic(session, NewsTable, URL, ID):
     
     
 
-    if(str(article.text)):
-        writeDescription(str(article.text), ID, session, NewsTable)
+    if(str(article_text)):
+        writeDescription(str(article_text), ID, session, NewsTable)
     else:
         print(f"description is empty. Url: {URL}")
         writeDescription("error while collecting", ID, session, NewsTable)
